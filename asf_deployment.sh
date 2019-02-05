e1726237c4b07bc82476dbebe6dea2282deb525e
TravorZhu <z2174305t@gmail.com>
2019-02-05 21:00:52 +0800

    Update asf_deployment.sh

--- asf_deployment.sh
+++ asf_deployment.sh
@@ -134,18 +134,18 @@ RHEL()
     yum -y install yum-utils
     touch /etc/yum.repos.d/nginx.repo
     echo -e "[nginx-stable]
-    name=nginx stable repo
-    baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
-    gpgcheck=1
-    enabled=1
-    gpgkey=https://nginx.org/keys/nginx_signing.key
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
 
-    [nginx-mainline]
-    name=nginx mainline repo
-    baseurl=http://nginx.org/packages/mainline/centos/\$releasever/\$basearch/
-    gpgcheck=1
-    enabled=0
-    gpgkey=https://nginx.org/keys/nginx_signing.key" >> /etc/yum.repos.d/nginx.repo
+[nginx-mainline]
+name=nginx mainline repo
+baseurl=http://nginx.org/packages/mainline/centos/\$releasever/\$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=https://nginx.org/keys/nginx_signing.key" >> /etc/yum.repos.d/nginx.repo
 
     yum-config-manager --enable nginx-mainline
     yum -y install nginx
